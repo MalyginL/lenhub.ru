5b8981ee735588faf9b5ed4bff28c59be207f493
MalyginL <lenyamalygin@gmail.com>
2018-09-01 18:59:30 +0300

    added background image

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewChecked} from '@angular/core';
 
 @Component({
     selector: 'login-page',
     templateUrl: './login-page.component.html',
     styleUrls: ['./login-page.component.scss']
   })
-  export class LoginPageComponent implements OnInit {
+  export class LoginPageComponent implements OnInit,AfterViewChecked {
+isLoading:boolean=true;
+
     ngOnInit(): void {
+      this.isLoading = true;
     }
+
+    ngAfterViewChecked() : void {
+      this.isLoading = false;
+  }
 
 constructor(){
 }
