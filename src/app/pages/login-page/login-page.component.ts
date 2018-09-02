cdcf285411b2953af5a1ea2b98a36d26ac929b30
MalyginL <lenyamalygin@gmail.com>
2018-09-03 01:16:26 +0300

    added animation + new fonts

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,21 @@
 import {Component, OnInit, AfterViewChecked} from '@angular/core';
 
 import {NgbModal,NgbModalRef, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import{
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 @Component({
     selector: 'login-page',
     templateUrl: './login-page.component.html',
-    styleUrls: ['./login-page.component.scss']
+    styleUrls: ['./login-page.component.scss'],
   })
-  export class LoginPageComponent implements OnInit,AfterViewChecked {
+
+export class LoginPageComponent implements OnInit,AfterViewChecked {
 isLoading:boolean=true;
 
 constructor(private modalService: NgbModal) {}
@@ -14,8 +23,10 @@ constructor(private modalService: NgbModal) {}
 private modalRef: NgbModalRef;
 
 openModal(content) {
+
   this.modalRef = this.modalService.open(content,{size:"sm"});
 }
+
 
 
 
